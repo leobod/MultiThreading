5f151af7d417d82f0f17e5ed90454624ce28bab7
Leobod <13952348337@sina.cn>
2020-03-24 19:43:29 +0800

    # %Name: C01T03LearnSleepYieldJoin # %Author: Leobod # %Contract: leobod@eside.cn # %Description: 添加对于线程的基本操作的练习Code

--- src/main/java/C01_BasicConcept/T03_LearSleepYieldJoin.java
+++ src/main/java/C01_BasicConcept/T03_LearSleepYieldJoin.java
@@ -3,6 +3,15 @@ package C01_BasicConcept;
 /**
  * 学习sleep、yield、join方法
  *
+ * Thread.sleep()  TimeUnit.Milliseconds.sleep()  单位毫秒
+ * 意思就是说，让线程休眠指定时间，休眠结束后运行
+ *
+ * Thread.yield()
+ * 意思就是说，让线程回到等待的队列，让线程重新去系统的调度里面来获取执行权限
+ *
+ * Thread.join()
+ * 意思就是说，当有2个以上的线程的时候
+ * 假设t1,t2，当在t2的某一处调用t1.join(),则t2要等t1运行结束后才能运行
  */
 
 public class T03_LearSleepYieldJoin {
@@ -69,6 +78,8 @@ public class T03_LearSleepYieldJoin {
     }
 
     public static void main(String[] args) {
-        testSleep();
+//        testSleep();
+        testYield();
+//        testJoin();
     }
 }
