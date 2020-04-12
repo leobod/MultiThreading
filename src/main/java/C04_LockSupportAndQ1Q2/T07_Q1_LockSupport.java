3fe99dc7a279392328ec1a7f6eafc9bd51b5c24e
Leobod <13952348337@sina.cn>
2020-04-12 17:56:50 +0800

    # %Name: Add-C04T07 Modify-C04T06 # %Author: Leobod # %Contract: leobod@eside.cn # %Description: 使用LockSupport来辅助解决Q1问题，修改CountDownLatch的注释，并添加改进.

--- src/main/java/C04_LockSupportAndQ1Q2/T07_Q1_LockSupport.java
+++ src/main/java/C04_LockSupportAndQ1Q2/T07_Q1_LockSupport.java
@@ -3,23 +3,16 @@ package C04_LockSupportAndQ1Q2;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.LockSupport;
 
 /**
- * 使用CountDownLatch
+ * 使用LockSupport来解决Q1
  *
- * 注释掉 sleep 前后，运行结果 偏了一位， 而且latch打开后结果继续运行
- *
- * 不sleep 结果为 5 后打印 t2 end, 实时性有待改进
- *
- * sleep 后结果为 4 后打印 t2 end
- *
- * 改进： 向modifysynchronized一样，在t1加 latch.await()
- *
- * 改进段，加来注释，但是效果并不明显，还需分析
+ * 貌似 刻意化， 增加了复杂度
  *
  */
 
-public class T06_Q1_CountDownLatch {
+public class T07_Q1_LockSupport {
     volatile List lists = new ArrayList();
 
     public void add(Object o) {
@@ -31,22 +24,22 @@ public class T06_Q1_CountDownLatch {
     }
 
 
+    static Thread t1 = null, t2 = null;
 
     public static void main(String[] args) {
-        T06_Q1_CountDownLatch t = new T06_Q1_CountDownLatch();
+        T07_Q1_LockSupport t = new T07_Q1_LockSupport();
 
         CountDownLatch latch = new CountDownLatch(1);
 
-        new Thread(() -> {
+        t2 = new Thread(() -> {
             if (t.size() != 5) {
-                try {
-                    latch.await();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                LockSupport.park();
             }
             System.out.println("t2 end");
-        }, "t2").start();
+            LockSupport.unpark(t1);
+        }, "t2");
+
+        t2.start();
 
         try {
             Thread.sleep(1);
@@ -54,18 +47,14 @@ public class T06_Q1_CountDownLatch {
             e.printStackTrace();
         }
 
-        new Thread(() -> {
+        t1 = new Thread(() -> {
             for (int i = 0; i < 10; ++i) {
                 t.add(new Object());
                 System.out.println("add " + i);
 
                 if (t.size() == 5) {
-                    latch.countDown();
-//                    try {
-//                        latch.await();
-//                    } catch (InterruptedException e) {
-//                        e.printStackTrace();
-//                    }
+                    LockSupport.unpark(t2);
+                    LockSupport.park();
                 }
 
                 try {
@@ -74,6 +63,8 @@ public class T06_Q1_CountDownLatch {
                     e.printStackTrace();
                 }
             }
-        }, "t1").start();
+        }, "t1");
+
+        t1.start();
     }
 }
